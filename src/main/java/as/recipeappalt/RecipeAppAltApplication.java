ac68ff4f3ea6ca7bf627c0452d19ea1c881b16df
Alex S <as202009261140@gmail.com>
2021-04-18 08:08:44 +0300

    init commit

---
+++ src/main/java/as/recipeappalt/RecipeAppAltApplication.java
@@ -0,0 +1,13 @@
+package as.recipeappalt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeAppAltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeAppAltApplication.class, args);
+	}
+
+}
---
+++ src/test/java/as/recipeappalt/RecipeAppAltApplicationTests.java
@@ -0,0 +1,13 @@
+package as.recipeappalt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAppAltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
